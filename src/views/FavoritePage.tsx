6551df2fb1383b58bbaf8ef25087bfac47972f71
camilo9090 <camilou9090@gmail.com>
2025-02-15 12:06:54 -0500

    agregar visualización de recetas favoritas; implementar lógica para mostrar mensaje cuando no hay favoritos

--- src/views/FavoritePage.tsx
+++ src/views/FavoritePage.tsx
@@ -1,7 +1,31 @@
+import { useMemo } from "react"
+import DrinkCard from "../componets/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 
 export default function FavoritePage() {
+
+  const favorites = useAppStore(state => state.favorites)
+  const hasFavorites=useMemo(()=>favorites.length,[favorites])
   return (
-    <h1>Favoritos</h1>
+    <>
+      <h1 className="text-6xl font-extrabold">Favoritos</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-10 p-20">
+
+        {hasFavorites?(
+          favorites.map(recipe => (
+            <DrinkCard 
+            key={recipe.idDrink}
+            drink={recipe}
+            
+            />
+  
+          ))
+        ):(
+          <p className="my-10 text-2xl text-center">Los Favoritos se muestran aquí</p>
+        )}
+      </div>
+
+    </>
   )
 }
